93b36f92ce89d3d59ff2c54bbfa6587d49c1133d
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2024-06-05 15:20:17 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Full",
+    products: [
+        .library(
+            name: "Full",
+            targets: ["Full"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Full", url: "https://pods.regulaforensics.com/Full/7.2.9102/DocumentReaderCore_full_7.2.9102.zip", checksum: "71e8fed18b5cec896cfabf497495c810883a45997af4058c02c7a657fc80d4e2"),
+    ]
+)
